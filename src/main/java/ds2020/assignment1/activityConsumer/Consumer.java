44a898b1442484bd10c62583282d0adf84a8436f
Tamas Pentek <pentek_pityu@yahoo.com>
2021-01-13 20:39:36 +0000

    Update Consumer.java

--- src/main/java/ds2020/assignment1/activityConsumer/Consumer.java
+++ src/main/java/ds2020/assignment1/activityConsumer/Consumer.java
@@ -91,7 +91,7 @@ public class Consumer {
         this.simpMessagingTemplate.convertAndSend("/queue/caregiver", "Patient: " + patientName + message);
     }
 
-    @MessageMapping("/api/caregiverInfo")
+    @MessageMapping("/caregiverInfo")
     public void getCurrentCaregiver(String caregiverMessage) throws JSONException {
         JSONObject caregiverObject = new JSONObject(caregiverMessage);
         this.currentCaregiverID = UUID.fromString(caregiverObject.getString("caregiverID"));
